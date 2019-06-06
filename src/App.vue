4d208443876a67d3d53f35a47032e64a2fc1f0b5
Drego31 <dominik.betka@arbuz.team>
2019-06-06 20:57:19 +0200

    adding m-footer

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
   <div id="app">
     <m-header/>
     <router-view/>
+    <m-footer/>
   </div>
 </template>
 
 <script>
   import MHeader from "./components/molecules/Header"
+  import MFooter from "./components/molecules/Footer"
   export default {
-    components: {MHeader}
+    components: {MFooter, MHeader}
   }
 </script>
---
+++ src/components/molecules/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="m-footer">
+    <div>
+      Powered by PGS
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "m-footer"
+  }
+</script>
