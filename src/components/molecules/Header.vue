32b091052ed593ca8212f01b6cb83ce648ede9a7
Drego31 <dominik.betka@arbuz.team>
2019-06-06 20:46:12 +0200

    adding m-header

---
+++ src/components/molecules/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="m-header">
+    <router-link to="/">About Us</router-link>
+    <router-link to="/skicams">Skicams</router-link>
+    <router-link to="/contact">Contact</router-link>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "m-header"
+  }
+</script>
