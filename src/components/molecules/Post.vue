32a8d3ce583320cd9a45b95b6774b326d7ce8730
Drego31 <dominik.betka@arbuz.team>
2019-06-07 19:41:39 +0200

    adding m-post and using that

---
+++ src/components/molecules/Post.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div>
+      <img :src="img" :alt="title">
+    </div>
+    <div>
+      <div>
+        {{ title }}
+      </div>
+      <div>
+        {{ message }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "m-post",
+  props: {
+    img: {type: String, required: true},
+    title: {type: String, required: true},
+    message: {type: String, required: true},
+  },
+}
+</script>
--- src/components/pages/AboutUs.vue
+++ src/components/pages/AboutUs.vue
@@ -1,13 +1,37 @@
 <template>
-  <t-page class="home">
-    AboutUs
+  <t-page class="p-about-us">
+    <m-post
+      v-for="(single, key) in posts"
+      :key="key"
+      v-bind="single"
+    />
   </t-page>
 </template>
 
 <script>
 import TPage from "../templates/Page"
+import MPost from "../molecules/Post"
 export default {
   name: 'p-about-us',
-  components: {TPage},
+  components: {MPost, TPage},
+  data: () => ({
+    posts: [
+      {
+        img: 'photo.jpg',
+        title: 'Lorem ipsum',
+        message: 'Etiam ullamcorper. Suspendisse a pellentesque dui, non felis meacenas.',
+      },
+      {
+        img: 'photo.jpg',
+        title: 'Lorem ipsum',
+        message: 'Etiam ullamcorper. Suspendisse a pellentesque dui, non felis meacenas.',
+      },
+      {
+        img: 'photo.jpg',
+        title: 'Lorem ipsum',
+        message: 'Etiam ullamcorper. Suspendisse a pellentesque dui, non felis meacenas.',
+      },
+    ],
+  }),
 }
 </script>
