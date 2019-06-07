114feabb3b753d105c0d83dd15959aa20270149e
Drego31 <dominik.betka@arbuz.team>
2019-06-07 22:29:56 +0200

    adding the downloading data

--- src/components/molecules/post/CamsView.vue
+++ src/components/molecules/post/CamsView.vue
@@ -2,23 +2,30 @@
   <div class="m-post f-cams-view">
     <div class="f-py-2 f-text-center">
       <span class="f-text-28">
-        {{ title }}
+        {{ name }}
       </span>
       <div class="a-date">
-        {{ date.format('DD-MM-YYYY') }}
+        {{ date }}
       </div>
     </div>
-    <div class="a-img f-cams-view" :style="`background-image: url('${img}');`"></div>
+    <div class="a-img f-cams-view" :style="`background-image: url('${cams[0].url}');`"></div>
+    <div class="a-img f-cams-view" :style="`background-image: url('${cams[1].url}');`"></div>
   </div>
 </template>
 
 <script>
+import moment from 'moment'
 export default {
   name: 'm-post-cams-view',
   props: {
-    img: {type: String, required: true},
-    title: {type: String, required: true},
-    date: {type: Object, required: true},
+    name: {type: String, required: true},
+    cams: {type: Array, required: true},
   },
+  data: () => ({
+    date: moment().format('DD-MM-YYYY')
+  }),
+  mounted() {
+    console.log({cams: this.cams})
+  }
 }
 </script>
--- src/components/pages/Skicams.vue
+++ src/components/pages/Skicams.vue
@@ -1,16 +1,16 @@
 <template>
   <t-page class="p-skicams">
     <m-post-cams-view
-      v-for="view in listOfViews"
-      v-bind="view"
+      v-for="(view, key) in listOfViews"
+      :key="key"
+      :name="view.name"
+      :cams="view.cams"
     />
   </t-page>
 </template>
 
 <script>
 import TPage from '../templates/Page'
-import moment from 'moment'
-import Image from '../../assets/logo.png'
 import MPostCamsView from '../molecules/post/CamsView'
 
 export default {
@@ -18,9 +18,31 @@ export default {
   components: {MPostCamsView, TPage},
   data: () => ({
     listOfViews: [
-      {img: Image, title: 'xxx', date: moment()},
-      {img: Image, title: 'xxx2', date: moment()},
+      {name: '', cams: [{url: ''}, {url: ''}]},
     ]
   }),
+  mounted() {
+    fetch('https://makevoid-skicams.p.mashape.com/cams.json', {
+      method: 'GET',
+      headers: {
+        'X-Mashape-Key': 'kxSXmUymofmshFHhhKxWOSJpqJsJp1I3zNnjsnqKwhITAiC1zw'
+      },
+    })
+      .then(response => response.json())
+      .then(jsonResponse => this.transformResponse(jsonResponse))
+      .catch(() => undefined)
+  },
+  methods: {
+    transformResponse(json) {
+      const array = Object.keys(json).map(key => {
+        json[key].cams = Object.keys(json[key].cams)
+          .map(cams_key => json[key].cams[cams_key])
+        return json[key]
+      })
+      this.listOfViews = array.filter(item => {
+        return item.name === 'Passo dello Stelvio' || item.name === 'Bielmonte'
+      })
+    },
+  },
 }
 </script>
